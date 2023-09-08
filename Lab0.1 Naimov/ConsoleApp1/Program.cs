ed8280e883e48bf1389c39f77f046770e0ffe1a8
Puja1379 <144465303+Puja1379@users.noreply.github.com>
2023-09-08 19:44:13 +0300

    Add files via upload

---
+++ Lab0.1 Naimov/ConsoleApp1/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение в радианах: ");
+        string input = Console.ReadLine();
+
+        if (double.TryParse(input, out double radians))
+        {
+            double pi = Math.PI;
+
+            double degrees = radians * (180 / pi);
+
+            Console.WriteLine($"{radians} радианов = {degrees} градусов");
+        }
+        else
+        {
+            Console.WriteLine("Некорректный ввод. Пожалуйста, введите число в радианах.");
+        }
+    }
+}
